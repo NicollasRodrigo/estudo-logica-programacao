857a28a6c97aa5c79331c8213c8e6074c5831e88
NicollasRodrigo <203463859+NicollasRodrigo@users.noreply.github.com>
2026-09-20 14:22:01 -0300

    feat: adicionar atividade de repeticao 1

---
+++ aulas/05-estruturas-de-repeticao/01_numeros_ate_zero.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main(void) {
+    int numero;
+    do {
+        printf("Digite um numero (0 para sair): ");
+        scanf("%d", &numero);
+        if (numero != 0) printf("Voce digitou: %d\n", numero);
+    } while (numero != 0);
+    printf("Programa encerrado.\n");
+    return 0;
+}
